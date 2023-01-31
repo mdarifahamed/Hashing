2e78ed4282022741fca162a5ac2391e2a69227c4
Arif <arifonly4@gmail.com>
2023-01-31 09:22:45 +0530

    Hashing

---
+++ IterationHashset.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class IterationHashset {
+    public static void main(String[] args) {
+        HashSet<String> cities = new HashSet<>();
+        cities.add("Delhi");
+        cities.add("Mumbai");
+        cities.add("Noida");
+        cities.add("Bengaluru");
+
+        // Iterator it = cities.iterator();
+        // while(it.hasNext()) {
+        //     System.out.println(it.next());
+        // }
+
+        for(String city: cities) {
+            System.out.println(city);
+        }
+    }
+}
